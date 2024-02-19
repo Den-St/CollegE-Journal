20f3a71daaa0362dc29368e7c7f56e5b955967bf
denisstuenko <stuenkden@gmail.com>
2024-02-19 21:19:26 +0200

    avatar crop changed

--- src/components/EditProfile/index.tsx
+++ src/components/EditProfile/index.tsx
@@ -106,7 +106,7 @@ export const EditProfile = () => {
         <div className='studentProfileInfo__container editProfileUserInfo'>
             <div className="editProfileChangePhoto_container">
                 <img className='studentProfile_img studentProfile_img_edit' src={newAvatarUrl || user.avatar || defaultAvatar}/>
-                <AntdImgCrop>
+                <AntdImgCrop showReset resetText="Відмінити зміни" rotationSlider showGrid modalTitle="Обробка фото" modalOk="Підтвердити" modalCancel="Відмінити">
                     <Upload beforeUpload={beforeUpload} accept="image/png, image/jpeg">
                         <Button className="uploadButton" icon={<UploadOutlined />}>Завантажити</Button>
                     </Upload>
