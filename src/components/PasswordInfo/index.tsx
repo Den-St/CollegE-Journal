aa6825551bd5eefe29db4bfe8776bb0d1eb4bc78
denisstuenko <stuenkden@gmail.com>
2024-02-29 13:46:59 +0200

    added _ to password

--- src/components/PasswordInfo/index.tsx
+++ src/components/PasswordInfo/index.tsx
@@ -9,7 +9,7 @@ export const PasswordInfo = () => {
         <ul className="passwordInfo_list">
             <li className="passwordInfo_listItem">Довжина пароля має бути від 8 до 30 символів</li>
             <li className="passwordInfo_listItem">Має містити лише літери латинського алфавіту та хоча б одну велику літеру</li>
-            <li className="passwordInfo_listItem">Має містити мінімум один спеціальний символ(!@#$%^&*+-/) та мінімум одну цифру</li>
+            <li className="passwordInfo_listItem">Має містити мінімум один спеціальний символ(!@#$%^&*+-/_) та мінімум одну цифру</li>
         </ul>
     </div>
 }
