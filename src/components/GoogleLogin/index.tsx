fb2d95b996c9549a8c70264e491e058bb446d6b5
denisstuenko <stuenkden@gmail.com>
2024-07-29 16:27:04 +0300

    google auth try 2

--- src/components/GoogleLogin/index.tsx
+++ src/components/GoogleLogin/index.tsx
@@ -4,6 +4,7 @@ import { URLSearchParams } from "url";
 import { setToken } from "../../helpers/auth";
 
 export const GoogleLogin = () => {
+    //https://collegejournal.ovh/api/google/login?state=I2MgIGjqlIEJmd2F201PMefRc2hz5w&code=4%2F0AcvDMrBlekEYvKitfhoACK-TCTtr7OriGEsRNWKvSdTjU8UK-EynnVxTXQXs36GJQKVpyw&scope=email+profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+openid&authuser=0&prompt=none
     const [searchParams] = useSearchParams();
     const token = searchParams.get('token');
     useEffect(() => {
@@ -12,6 +13,7 @@ export const GoogleLogin = () => {
         window.parent.postMessage(JSON.stringify({params: {token: searchParams.get('token')}}), "*");
         window.close();  
         //close iframe
-    }, [])
+    }, []);
+
     return <></>
 }
--- src/hooks/googleAuth.ts
+++ src/hooks/googleAuth.ts
@@ -1,28 +1,42 @@
+import { setToken } from './../helpers/auth';
+import { useSearchParams } from 'react-router-dom';
+import { useEffect } from 'react';
 import { endpoints } from './../consts/endpoints';
 import axiosConfig from '../axiosConfig';
 
 export const useGoogleAuth = () => {
+    const [searchParams,_] = useSearchParams();
+    const state = searchParams.get('state');
+    const code = searchParams.get('code');
+    const scope = searchParams.get('scope');
+    const authuser = searchParams.get('authuser');
+    const prompt = searchParams.get('prompt');
+    
     const onOpenAuthWindow = async () => {
         try{
             const res = await axiosConfig.get(endpoints.googleAuthGetUrl);
             console.log(res);
             const popup = window.open(res.data.redirect_url,'googleAuthPopup','width=600,height=400,left=200,top=200',);
-            
-            window.addEventListener('message', function(event) {
-                // Check the origin of the message for security reasons
-                if (event.origin !== res.data.redirect_url) {
-                    return;
-                }
-
-                // Handle the received data
-                console.log('Data received from popup:', event.data);
-
-                // You can now use the data in your main app
-            }, false);
+         
         }catch(err){
             console.error(err);
         }
     }
+    const onGoogleLogin =  async () => {
+        try{
+            const res = await axiosConfig.get(endpoints.googleLogin+`?state=${state}&code=${code}&scope=${scope}&authuser=${authuser}&prompt=${prompt}`);
+            if(!res.data.token) return;
+            setToken(res.data.token);
+            window.close();
+        }catch(err){
+            console.log(err);
+        }
+    }
+    // console.log(state , code , scope , authuser , prompt)
+    useEffect(() => {
+        if(!state || !code || !scope || authuser === null || !prompt) return;
+        onGoogleLogin();
+    },[state,code,scope,authuser,prompt])
 
     return {onOpenAuthWindow};
 }
