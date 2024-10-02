40cc98c65607eeb34f54615273bf96a7b99366b6
denisstuenko <stuenkden@gmail.com>
2024-10-02 10:00:28 +0300

    syncronized table, teachers and subject at absence table page

--- src/components/Journal/AbsenceTable/AbsenceTable.tsx
+++ src/components/Journal/AbsenceTable/AbsenceTable.tsx
@@ -1,5 +1,5 @@
 import { Select, Spin } from "antd";
-import {  useEffect, Fragment,  } from "react";
+import {  useEffect, Fragment, forwardRef, RefObject,  } from "react";
 import { LinkBack } from "../../../assets/components/LinkBack/LinkBack";
 import { FilterIconSvg } from "../../../assets/svgs/filterIconSvg";
 import { JournalPortraitModeWarning } from "../../../assets/svgs/journalPortraitModeWarningSvg";
@@ -8,7 +8,6 @@ import { RightArrowSvg } from "../../../assets/svgs/rightArrowSvg";
 import { routes } from "../../../consts/routes";
 import { daysShort } from "../../../helpers/daysShort";
 import { useGroupsByTeacher } from "../../../hooks/groupsByTeacher";
-import { useJournalDragScroll } from "../../../hooks/useJournalDragScroll";
 import { useThemeStore } from "../../../store/themeStore";
 import { useUserStore } from "../../../store/userStore";
 import { JournalGroupT } from "../../../types/journalGroup";
@@ -19,11 +18,12 @@ import './absenceTableStyles.scss';
 import { securityLevels } from "../../../consts/securityLevels";
 import { AbsenceTableFilltersT, AbsenceTableT } from "../../../types/absenceTable";
 import { useAbsenceTableDownload } from "../../../hooks/absenceTableDownload";
-import { useAbsenceTableTeacherSubjectsDragToScroll } from "../../../hooks/absenceTableDragToScroll";
 import { useAbsenceTablePrintForm } from "../../../hooks/absenceTablePrintform";
 import { useGetAbsenceTable } from "../../../hooks/getAbsenceTable";
 import {DoubleRightOutlined} from '@ant-design/icons';
 import { AbsenceTablePrintForm } from "./AbsenceTablePrintForm";
+import React from "react";
+import { useAbsenceTableDragScroll } from "../../../hooks/absenceTableDragScroll";
 const {Option} = Select;
 
 
@@ -33,7 +33,8 @@ export const AbsenceTable = () => {
     const group = groups.find(group => group.journal_group === fillters.group_id);
     const theme = useThemeStore().theme;
     const {cellsRef,lessonTypesRef,mainContainerRef,onMouseMove,mouseUpHandler,
-           mouseDownHandler,handleHorizontalScrollLessonTypes,handleHorizontalScroll,handleVerticalScroll} = useJournalDragScroll();
+           mouseDownHandler,handleHorizontalScroll,
+           handleVerticalScroll,teachersRef,subjectsRef} = useAbsenceTableDragScroll();
     const user_level = useUserStore().user.security_level;
 
     useEffect(() => {
@@ -64,7 +65,7 @@ export const AbsenceTable = () => {
                     <h1 className='journalColumnsLeft__title'>Цитати на кожен день</h1>
                     <p className='journalColumnsLeft__text'>У жовтні кожного року проходить акція «відрахуй випускника»</p>
                 </div>
-                <div className='journalColumnsCenter__container' style={{'gap':'21px'}} onScroll={handleHorizontalScrollLessonTypes} ref={lessonTypesRef}>
+                <div className='journalColumnsCenter__container' style={{'gap':'21px'}} onScroll={() => handleHorizontalScroll("lessonTypes")} ref={lessonTypesRef}>
                     {table.dates?.map((date,i) => 
                         <div key={date+i}  className={`absenceTable_dayContainer`}>
                             <p className="absenceTable_day">{daysShort[date.split(' ')[0]]} <br/> {date.split(' ')[1]}</p>
@@ -95,7 +96,7 @@ export const AbsenceTable = () => {
                 <div className='journalRightRowsContainer'
                     onMouseDown={mouseDownHandler} onMouseUp={mouseUpHandler}
                     onMouseMove={onMouseMove}
-                    ref={cellsRef} onScroll={handleHorizontalScroll}>
+                    ref={cellsRef} onScroll={() => handleHorizontalScroll("cells")}>
                     {table?.student_list.map((student,i) => 
                         <div key={student.full_name+i} style={{'height':'30px','marginLeft':'1px'}} className={`journalRowItem__container ${(i+1)%2 === 0 ? 'even' : ''}`}>
                             <div className='journalRowItemCenter__container' style={{'gap':'10px','marginLeft':'65px'}}>
@@ -125,28 +126,36 @@ export const AbsenceTable = () => {
                 </div>
             </div>
         </section>
-        <AbsenceTableTeachersSubjects table={table}/>
+        <AbsenceTableTeachersSubjects dragScroll={{'onMouseDown':mouseDownHandler,'onMouseMove':onMouseMove,'onMouseUp':mouseUpHandler,'onScroll':handleHorizontalScroll}} refs={{teachersRef,subjectsRef}} table={table}/>
         </>}
     </div>
 }
 
 type TeachersProps = {
-    table:AbsenceTableT
+    table:AbsenceTableT,
+    dragScroll:{
+        onMouseDown:(e:React.MouseEvent<HTMLDivElement,MouseEvent>) => void,
+        onMouseUp:() => void,
+        onMouseMove:(e:React.MouseEvent<HTMLDivElement,MouseEvent>,localIsMouseDown?:boolean,localMousePos?:{x:number,y:number}) => void,
+        // ref={cellsRef},
+        onScroll:(element:"cells" | "lessonTypes" | "teachers" | "subjects") => void
+    },
+    refs:{
+        teachersRef:RefObject<HTMLDivElement>,
+        subjectsRef:RefObject<HTMLDivElement>
+    }
 }
 
-const AbsenceTableTeachersSubjects:React.FC<TeachersProps> = ({table}) => {
-    const {teachersRef,subjectsRef,onMouseMove,mouseUpHandler,
-        mouseDownHandler,handleHorizontalScrollTeachers,handleHorizontalScrollSubjects} = useAbsenceTableTeacherSubjectsDragToScroll();
-
+const AbsenceTableTeachersSubjects = forwardRef<HTMLDivElement,TeachersProps>(({dragScroll,table,refs},ref) => {
     return  <section 
-            onMouseUp={mouseUpHandler}
+            onMouseUp={dragScroll.onMouseUp}
             className="absenceTable_teacherSection">
         <div className="absenceTable_teachersContainer">
             <p className="absenceTable_teachersSection_header">ПІБ Викладача</p>
             <div 
-            onScroll={handleHorizontalScrollTeachers} ref={teachersRef}
-            onMouseDown={mouseDownHandler} onMouseUp={mouseUpHandler}
-            onMouseMove={onMouseMove}
+            onScroll={() => dragScroll.onScroll("teachers")} ref={refs.teachersRef}
+            onMouseDown={dragScroll.onMouseDown} onMouseUp={dragScroll.onMouseUp}
+            onMouseMove={dragScroll.onMouseMove}
             className="absenceTable_teachersContainer">
                 {table.teachers.map((teachersSubArray,i) => <div key={teachersSubArray.join(' ')+i} className="absenceTable_teachersSubcontainer">{teachersSubArray.map((teacher,j) => <p key={teacher+i+','+j} className="absenceTable_teacher">{teacher}</p>)}</div>)}
             </div>
@@ -154,15 +163,15 @@ const AbsenceTableTeachersSubjects:React.FC<TeachersProps> = ({table}) => {
         <div className="absenceTable_teachersContainer">
             <p className="absenceTable_teachersSection_header">Дисципліна</p>
             <div 
-            onScroll={handleHorizontalScrollSubjects} ref={subjectsRef} 
-            onMouseDown={mouseDownHandler} onMouseUp={mouseUpHandler}
-            onMouseMove={onMouseMove}
+            onScroll={() => dragScroll.onScroll("subjects")} ref={refs.subjectsRef} 
+            onMouseDown={dragScroll.onMouseDown} onMouseUp={dragScroll.onMouseUp}
+            onMouseMove={dragScroll.onMouseMove}
             className="absenceTable_teachersContainer">
                 {table.subjects.map((subjectsSubArray,i) => <div key={subjectsSubArray.join(' ')+i} className="absenceTable_teachersSubcontainer">{subjectsSubArray.map((subject,j) => <p key={subject+i+','+j} className="absenceTable_teacher">{subject}</p>)}</div>)}
             </div>
         </div>
     </section>
-}
+})
 
 type Props = {
     fillters:AbsenceTableFilltersT,
